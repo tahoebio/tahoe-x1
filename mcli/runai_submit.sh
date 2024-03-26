70b9849fcd7fb8f2c3c671a74c621cc9f6005cbe
Shreshth Gandhi <shreshth@vevo.ai>
2024-03-26 19:06:53 -0400

    Change binning default to right-sided and add parameter file for RunAI

--- mcli/runai_submit.sh
+++ mcli/runai_submit.sh
@@ -2,4 +2,5 @@ pip install llm-foundry==0.6.0 mosaicml[deepspeed]
 cd /src/vevo-scGPT
 pip install -e .
 cd examples
-composer pretrain_composer.py
+composer pretrain_composer.py /src/vevo-scGPT/runai/scgpt-50m-train.yaml
+```
