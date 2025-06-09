1383fdecf75fd2ca240b7f4c9e6d662244293fc0
farjvd <167781123+farjvd@users.noreply.github.com>
2025-06-09 19:52:59 -0400

    Updates to cell-classification calback(#85)
    * refactored cell_classification benchmark to support Tahoe and Tabula dataset
    
    * added default values for gene_name_key and cell_type_key!
    
    * Added skip_clustering flag!
    
    * added config file!
    
    * added comments!
    
    * converted compute_lisi_core function to pytorch
    
    * ✨Modify cell-classification callback
    Changes:
     - Test split is optional (eg: for CCLE)
     - Option to skip clustering is removed for now
     - ensembl_to_gene_id dict is used, gene_name key is changed to gene_id key
     - class_names is used directly instead of converting to numerical values
     - batch size and seq len can be specified per dataset, otherwise a default is used.
    
    * 🐛 LISI score should use cell_type key, only "train" and "test" splits should be allowed for download
    
    * 🐛 cell_type_key should be used in UMAP
    
    * 🐛 cell_type_key should be fetched from obs
    
    * 📝 Add configs for test run
    
    ---------
    
    Co-authored-by: Shreshth Gandhi <shreshth@vevo.ai>

--- mosaicfm/tasks/cell_classification.py
+++ mosaicfm/tasks/cell_classification.py
@@ -1,6 +1,6 @@
 # Copyright (C) Vevo Therapeutics 2025. All rights reserved.
 import io
-import json
+from typing import Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,15 +9,14 @@ import torch
 from composer import State
 from composer.core.callback import Callback
 from composer.loggers import Logger
-from composer.utils import dist, model_eval_mode
+from composer.utils import model_eval_mode
 from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import kneighbors_graph
+from sklearn.metrics import f1_score
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from mosaicfm.utils import download_file_from_s3_url
 
 
-# Custom Callback to run the cell classification after training
 class CellClassification(Callback):
     def __init__(
         self,
@@ -27,70 +26,58 @@ class CellClassification(Callback):
         super().__init__()
 
         self.dataset_registry = cfg.get("datasets")
-        self.logistic_cfg = cfg.get("logistic")
+        self.classifier_config = cfg.get("classifier_config")
         self.batch_size = cfg.get("batch_size", 50)
         self.seq_len = cfg.get("seq_len", 2048)
-
-        # load gene_to_id mapping
-        assert (
-            "ensemble_to_gene_path" in cfg
-        ), "ensemble_to_gene_path not found in config and should be provided!"
-        ensemble_to_gene_path = cfg.get("ensemble_to_gene_path")
-
-        if dist.get_local_rank() == 0:
-            download_file_from_s3_url(
-                s3_url=ensemble_to_gene_path["remote"],
-                local_file_path=ensemble_to_gene_path["local"],
-            )
-        with dist.local_rank_zero_download_and_wait(
-            ensemble_to_gene_path["local"],
-        ):
-            dist.barrier()
-
-        with open(ensemble_to_gene_path["local"]) as f:
-            id_to_gene = json.load(f)
-        self.gene_to_id = dict(zip(id_to_gene.values(), id_to_gene.keys()))
 
     def fit_end(self, state: State, logger: Logger):
 
         self.model = state.model
-
         self.model_config = self.model.model_config
         self.collator_config = self.model.collator_config
         self.vocab = state.train_dataloader.collate_fn.vocab
         self.run_name = state.run_name
 
-        # cell classification both for zheng and Segerstolpe datasets
-        for datast_name, dataset_cfg in self.dataset_registry.items():
-
-            # download dataset splits
-            for split in dataset_cfg:
-                download_file_from_s3_url(
-                    s3_url=dataset_cfg[split]["remote"],
-                    local_file_path=dataset_cfg[split]["local"],
-                )
-
-            self.cell_classfication(datast_name, logger)
+        for dataset_name, dataset_cfg in self.dataset_registry.items():
+            for split in ["train", "test"]:
+                if split in dataset_cfg:
+                    download_file_from_s3_url(
+                        s3_url=dataset_cfg[split]["remote"],
+                        local_file_path=dataset_cfg[split]["local"],
+                    )
+            self.cell_classfication(dataset_name, logger)
 
     def cell_classfication(self, dataset: str, logger: Logger):
-        # step 1: load data train, test
-        class_idx_to_name = np.load(
-            self.dataset_registry[dataset]["class_names"]["local"],
+        cell_type_key = self.dataset_registry[dataset].get(
+            "cell_type_key",
+            "cell_type_label",
         )
-        adata_train, gene_ids_train, labels_train, _ = (
-            self.prepare_cell_annotation_data(
-                self.dataset_registry[dataset]["train"]["local"],
-                class_idx_to_name,
+        gene_id_key = self.dataset_registry[dataset].get(
+            "gene_id_key",
+            "ensembl_id",
+        )
+        adata_train, gene_ids_train = self.prepare_cell_annotation_data(
+            self.dataset_registry[dataset]["train"]["local"],
+            cell_type_key=cell_type_key,
+            gene_id_key=gene_id_key,
+        )
+        use_test_split = False
+        if "test" in self.dataset_registry[dataset]:
+            adata_test, gene_ids_test = self.prepare_cell_annotation_data(
+                self.dataset_registry[dataset]["test"]["local"],
+                cell_type_key=cell_type_key,
+                gene_id_key=gene_id_key,
             )
-        )
-        adata_test, gene_ids_test, labels_test, _ = self.prepare_cell_annotation_data(
-            self.dataset_registry[dataset]["test"]["local"],
-            class_idx_to_name,
-        )
+            use_test_split = True
 
         # step 2: extract mosaicfm embeddings
         from mosaicfm.tasks import get_batch_embeddings
 
+        dataset_batch_size = self.dataset_registry[dataset].get(
+            "batch_size",
+            self.batch_size,
+        )
+        dataset_seq_len = self.dataset_registry[dataset].get("seq_len", self.seq_len)
         with model_eval_mode(
             self.model.model,
         ), torch.no_grad(), FSDP.summon_full_params(self.model.model, writeback=False):
@@ -102,42 +89,46 @@ class CellClassification(Callback):
                 gene_ids=gene_ids_train,
                 model_cfg=self.model_config,
                 collator_cfg=self.collator_config,
-                batch_size=self.batch_size,
-                max_length=self.seq_len,
+                batch_size=dataset_batch_size,
+                max_length=dataset_seq_len,
                 return_gene_embeddings=False,
             )
-            cell_embeddings_test = get_batch_embeddings(
-                adata=adata_test,
-                model=self.model.model,
-                vocab=self.vocab,
-                gene_ids=gene_ids_test,
-                model_cfg=self.model_config,
-                collator_cfg=self.collator_config,
-                batch_size=self.batch_size,
-                max_length=self.seq_len,
-                return_gene_embeddings=False,
+            if use_test_split:
+                cell_embeddings_test = get_batch_embeddings(
+                    adata=adata_test,
+                    model=self.model.model,
+                    vocab=self.vocab,
+                    gene_ids=gene_ids_test,
+                    model_cfg=self.model_config,
+                    collator_cfg=self.collator_config,
+                    batch_size=dataset_batch_size,
+                    max_length=dataset_seq_len,
+                    return_gene_embeddings=False,
+                )
+
+        if use_test_split:
+            # step 3: train classifier if test split is available
+            clf = LogisticRegression(
+                max_iter=self.classifier_config.get("max_iter", 5000),
+                solver=self.classifier_config.get("solver", "lbfgs"),
+                multi_class=self.classifier_config.get("multi_class", "multinomial"),
+                random_state=self.classifier_config.get("random_state", 42),
             )
+            clf.fit(cell_embeddings_train, adata_train.obs[cell_type_key].values)
 
-        # step 3: train classifier
-        clf = LogisticRegression(
-            max_iter=5000,
-            solver="lbfgs",
-            multi_class="multinomial",
-            random_state=42,
-        )
-        clf.fit(cell_embeddings_train, labels_train)
+            # step 4: calculate and log metrics
+            labels_pred = clf.predict(cell_embeddings_test)
+            f1 = f1_score(
+                adata_test.obs[cell_type_key].values,
+                labels_pred,
+                average="macro",
+            )
+            logger.log_metrics({f"macro_f1_{dataset}": f1})
 
-        # step 4: calculate and log metrics
-        from sklearn.metrics import f1_score
-
-        labels_pred = clf.predict(cell_embeddings_test)
-        f1 = f1_score(labels_test, labels_pred, average="macro")
-        logger.log_metrics({f"macro_f1_{dataset}": f1})
-
-        # step 5: compute lisi
+        # Step 5: compute LISI score for train split
         lisi_score = self.compute_lisi_scores(
             cell_embeddings_train,
-            adata_train.obs["cell_type_label"].values,
+            adata_train.obs[cell_type_key].values.to_numpy(dtype="str"),
             20,
         )
         logger.log_metrics({f"LISI {dataset}": lisi_score})
@@ -148,7 +139,7 @@ class CellClassification(Callback):
         sc.tl.umap(adata_train)
         fig = sc.pl.umap(
             adata_train,
-            color=["cell_type_names"],
+            color=[cell_type_key],
             frameon=False,
             title=[f"{self.run_name} LISI:{lisi_score:.2f} \n {dataset} Dataset"],
             return_fig=True,
@@ -161,63 +152,68 @@ class CellClassification(Callback):
         img = np.array(plt.imread(buf))
         logger.log_images(img, name=f"clustering_{dataset}", channels_last=True)
 
-    def compute_lisi_scores(self, emb: np.ndarray, labels: np.ndarray, k: int):
-        nng = kneighbors_graph(emb, n_neighbors=k).tocoo()
-        labels = np.unique(labels, return_inverse=True)[1]
-        self_id = labels[nng.row]
-        ne_id = labels[nng.col]
-
-        _, c = np.unique(labels, return_counts=True)
-        theoretic_score = ((c / c.sum()) ** 2).sum()
-        return (self_id == ne_id).mean() / theoretic_score
-
     def prepare_cell_annotation_data(
         self,
         data_path: str,
-        class_idx_to_name: np.ndarray,
+        gene_id_key: str,
+        cell_type_key: str,
     ):
 
         vocab = self.vocab
         adata = sc.read_h5ad(data_path)
-
-        gene_name_key = "gene_symbols"
-        gene_col = "gene_id"
-        cell_type_key = "cell_type_label"
-
-        adata.var[gene_col] = adata.var[gene_name_key].apply(
-            lambda x: self.gene_to_id.get(x, "na"),
-        )
-
-        # filter the cell with NaN values in the cell_type_key
         adata = adata[~adata.obs[cell_type_key].isna(), :]
-        adata.obs["cell_type_names"] = [
-            class_idx_to_name[int(id)] for id in adata.obs[cell_type_key]
+
+        adata.var["id_in_vocab"] = [
+            vocab[gene] if gene in vocab else -1 for gene in adata.var[gene_id_key]
         ]
-        adata.var["id_in_vocab"] = [vocab[gene] for gene in adata.var[gene_col]]
         gene_ids_in_vocab = np.array(adata.var["id_in_vocab"])
         print(
             f"match {np.sum(gene_ids_in_vocab >= 0)}/{len(gene_ids_in_vocab)} genes "
             f"in vocabulary of size {len(vocab)}.",
         )
         adata = adata[:, adata.var["id_in_vocab"] >= 0]
-        vocab.default_index = vocab["<pad>"]
-        genes = adata.var[gene_col].tolist()
+        genes = adata.var[gene_id_key].tolist()
         gene_ids = np.array([vocab[gene] for gene in genes], dtype=int)
 
-        # Extract labels from the AnnData object
-        labels = adata.obs[cell_type_key].values
-        unique_labels = np.unique(np.array(labels[~np.isnan(np.array(labels))]))
+        return adata, gene_ids
 
-        # Convert labels to numeric if they are not already
-        if labels.dtype.kind in "OU":  # Object or Unicode, meaning strings
-            label_names = labels
-            label_to_idx = {label: idx for idx, label in enumerate(unique_labels)}
-            labels = np.array([label_to_idx[label] for label in labels])
-        else:
-            labels = labels.astype(np.int64)
-            label_names = np.array([class_idx_to_name[label] for label in labels])
+    @staticmethod
+    def compute_lisi_scores(
+        emb: Union[np.ndarray, torch.Tensor],
+        labels: Union[np.ndarray, torch.Tensor],
+        k: int,
+    ) -> float:
+        """Computes a LISI score. Accepts numpy arrays or torch tensors.
 
-        adata = adata.copy()
-        adata.X = adata.X.todense()
+        Args:
+            emb (Union[np.ndarray, torch.Tensor]): (n_samples, n_features) embedding matrix.
+            labels (Union[np.ndarray, torch.Tensor]): (n_samples,) label vector, can be strings or ints.
+            k (int): Number of neighbors.
 
-        return adata, gene_ids, labels, label_names
+        Returns:
+            float: The LISI score.
+        """
+        # Convert to torch tensors
+        emb = torch.from_numpy(emb).float()
+        _, inverse_labels = np.unique(labels, return_inverse=True)
+        labels = torch.from_numpy(inverse_labels).long()
+
+        # Compute pairwise distances
+        distances = torch.cdist(emb, emb, p=2)
+
+        # Get k nearest neighbors for each point (excluding itself)
+        _, knn_indices = torch.topk(distances, k + 1, largest=False)
+        knn_indices = knn_indices[:, 1:]  # exclude self
+
+        # Self vs neighbor labels
+        self_labels = labels.unsqueeze(1).expand(-1, k)
+        neighbor_labels = labels[knn_indices]
+
+        # Compute label agreement
+        same_label = (self_labels == neighbor_labels).float().mean()
+
+        # Theoretical LISI normalization
+        label_counts = torch.bincount(labels)
+        theoretic_score = ((label_counts / label_counts.sum()) ** 2).sum()
+
+        return (same_label / theoretic_score).item()
